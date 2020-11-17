00b19fd12e5c8872edad9ffe29657164f97eb054
Oleksandr Zenaiev <oleksandr.zenaiev@cern.ch>
2020-11-17 15:22:16 +0100

    u

--- pycode.py
+++ pycode.py
@@ -45,17 +45,26 @@ class Drop:
                 fig.canvas.set_window_title(title[i].split('[')[0])
                 plt.subplots_adjust(hspace = 0.0, left = 0.01, right = 0.99)
               ax = axes[i%3]
+              if i%3 == 2:
+                store = title[i].split('[')[0]
+              else:
+                store = None
             else:
               fig = plt.figure(title[i])
               ax = None
+              store = title[i]
             bkg_sample = random.sample(bkg, min(10000, len(bkg)))
-            cuts.append(self._plot(title[i], [v[i] for v in sig], [v[i] for v in bkg_sample], np.linspace(0, 300, 100), [1.0, 0.90], ax, col=['b','g','r'][i%3]))
-        print('cuts = {}'.format(cuts_xy))
-        print('cuts_x1 = {}'.format(cuts_x1))
+            cuts.append(self._plot(title[i], [v[i] for v in sig], [v[i] for v in bkg_sample], np.linspace(0, 300, 100), [1.0, 0.90], ax, col=['b','g','r'][i%3], store=store))
+        print('dropper.cuts = {}'.format(cuts_xy))
+        print('dropper.cuts_x1 = {}'.format(cuts_x1))
         plt.show(block=False)
         cv2.waitKey()
+        #print(self.cuts)
+        assert all(abs(v1-v2)<1e-5 for v1,v2 in zip(self.cuts, cuts_xy))
+        #print(self.cuts_x1)
+        assert all(abs(v1-v2)<1e-5 for v1,v2 in zip(self.cuts_x1, cuts_x1))
 
-    def _plot(self, label, sig, bkg, bins=None, qs=[0.90], ax=None, col=None):
+    def _plot(self, label, sig, bkg, bins=None, qs=[0.90], ax=None, col=None, store=False):
       #print(len(sig), len(bkg))
       sig_mean,sig_min,sig_max = sum(sig)/len(sig), min(sig), max(sig)
       sig_str = '{:5.0f} [{:5.0f}{:5.0f}]'.format(sig_mean,sig_min,sig_max)
@@ -81,6 +90,9 @@ class Drop:
       ax.legend(loc=2)
       ax.text(0.0,0.45,'\n'.join(q_str),horizontalalignment='left', verticalalignment='top', transform = ax.transAxes)
       ax.axes.get_yaxis().set_visible(False)
+      if store is not None:
+        plt.savefig('vars/{}.pdf'.format(store))
+        plt.savefig('vars/{}.png'.format(store))
       return sig_min
 
     def _calc_sig_bkg_eff(self, sig, bkg, q):
@@ -182,15 +194,16 @@ class Drop:
       num_rows, num_cols = marrsum.shape[:2]
       translation_matrix = np.float32([ [1,0,mar_r-1], [0,1,0] ])
       marrsum = cv2.warpAffine(marrsum, translation_matrix, (num_cols,num_rows))
-      marrsum = maxrgb(marrsum)      
-      self.vars_title += ['tsum_max'+'['+c+']' for c in 'bgr']
-      self.vars_title += ['tasum_max'+'['+c+']' for c in 'bgr']
-      self.vars_title += ['bsum_max'+'['+c+']' for c in 'bgr']
-      self.vars_title += ['basum_max'+'['+c+']' for c in 'bgr']
-      self.vars_title += ['lsum'+'['+c+']' for c in 'bgr']
-      self.vars_title += ['lasum'+'['+c+']' for c in 'bgr']
-      self.vars_title += ['boxsum_max']
-      self.vars_title += ['marlsum']
+      marrsum = maxrgb(marrsum)
+      if len(self.vars_title) == 0:
+        self.vars_title += ['tsum_max'+'['+c+']' for c in 'bgr']
+        self.vars_title += ['tasum_max'+'['+c+']' for c in 'bgr']
+        self.vars_title += ['bsum_max'+'['+c+']' for c in 'bgr']
+        self.vars_title += ['basum_max'+'['+c+']' for c in 'bgr']
+        self.vars_title += ['lsum'+'['+c+']' for c in 'bgr']
+        self.vars_title += ['lasum'+'['+c+']' for c in 'bgr']
+        self.vars_title += ['boxsum_max']
+        self.vars_title += ['marlsum']
       img_vars = cv2.merge(
         () +
         tuple(tsum_max[:-box_y-1, 1:-box_x, c] for c in range(img.shape[2])) +
@@ -203,9 +216,10 @@ class Drop:
         tuple((marlsum[1:-box_y, 1:-box_x]*-1,)) +
       ())
       #print(img[0:3, 0:3])
-      self.vars_title_x1 += ['rsum'+'['+c+']' for c in 'bgr']
-      self.vars_title_x1 += ['rasum'+'['+c+']' for c in 'bgr']
-      self.vars_title_x1 += ['marrsum']
+      if len(self.vars_title_x1) == 0:
+        self.vars_title_x1 += ['rsum'+'['+c+']' for c in 'bgr']
+        self.vars_title_x1 += ['rasum'+'['+c+']' for c in 'bgr']
+        self.vars_title_x1 += ['marrsum']
       img_vars_x1 = cv2.merge(
         () +
         tuple(rsum[1:-box_y, :-1, c] for c in range(img.shape[2])) +
@@ -362,7 +376,7 @@ class Drop:
 dropper = Drop()
 
 #def py_droprec(img, timestamp=0, y0=20, y1=570, x0=None, x1=None, signal=[]):
-def py_droprec(img, timestamp=0, y0=None, y1=None, x0=None, x1=None, signal=[]):
+def py_droprec(img, timestamp=0, signal=[], y0=None, y1=None, x0=None, x1=None):
   print('SZ droprec timestamp = {}, crop = [{}:{}, {}:{}], signal = {}'.format(timestamp, y0, y1, x0, x1, signal))
   signal_copy = signal.copy()
   for s in signal_copy:
@@ -408,26 +422,47 @@ def list_images(basePath, contains=None):
     # return the set of files that are valid
     return list_files(basePath, validExts=image_types, contains=contains)
 
-def get_img(img_name):
+GHP = 'Greater Healing Potion'
+GMP = 'Greater Mana Potion'
+SHP = 'Super Healing Potion'
+SMP = 'Super Mana Potion'
+FRP = 'Full Rejuvenation Potion'
+RP = 'Rejuvenation Potion'
+def get_img(img_name, signal=None):
   img = cv2.imread(img_name)
   timestamp = os.path.splitext(os.path.basename(img_name))[0]
-  return img, timestamp
+  if signal is None:
+    sig_file = os.path.splitext(img_name)[0] + '.txt'
+    if os.path.exists(sig_file):
+      print('reading signal from {}'.format(sig_file))
+      signal = []
+      with open(sig_file) as f:
+        for l in f.readlines():
+          y,x,x1,c = l.split(' ')[:4]
+          item = ' '.join(l.split(' ')[4:]).rstrip()
+          try:
+            eval(item)
+          except (NameError, SyntaxError):
+            pass
+          else:
+            item = eval(item)
+          signal.append([int(y), int(x), int(x1), item, c])
+  return img, timestamp, signal
 
 if __name__ == '__main__':
   np.set_printoptions(threshold=np.inf, linewidth=np.inf)
-  dropper.cuts = [-7.16129, -7.0, -10.4838705, -38.83871, -38.451614, -36.322582, -5.096774, -4.1827955, -5.419355, -21.655914, -20.903225, -22.838709, -39.5, -30.375, -22.625, -42.0, -33.625, -35.5, 44.62088, -19.088888]
-  dropper.cuts_x1 = [-9.1875, -12.9375, -12.875, -32.25, -37.25, -44.0625, -16.022223]
-  dropper.flag_train = 0
+  #dropper.cuts = [-7.16129, -7.0, -10.4838705, -38.83871, -38.451614, -36.322582, -5.096774, -4.1827955, -5.419355, -21.655914, -20.903225, -22.838709, -39.5, -30.375, -22.625, -42.0, -33.625, -35.5, 44.62088, -19.088888]
+  #dropper.cuts_x1 = [-9.1875, -12.9375, -12.875, -32.25, -37.25, -44.0625, -16.022223]
+  dropper.cuts = [-7.16129, -7.0, -10.4838705, -38.83871, -38.451614, -36.322582, -7.2645164, -6.0903225, -8.858065, -21.941935, -22.754839, -26.393549, -39.5, -30.375, -22.625, -42.0, -33.625, -35.5, 41.008244, -19.088888]
+  dropper.cuts_x1 = [-15.625, -16.5625, -13.1875, -32.25, -37.25, -44.0625, -16.022223]
+  dropper.flag_train = 1
   if len(sys.argv) == 1:
-    GHP = 'Greater Healing Potion'
-    GMP = 'Greater Mana Potion'
-    SHP = 'Super Healing Potion'
-    SMP = 'Super Mana Potion'
-    FRP = 'Full Rejuvenation Potion'
-    RP = 'Rejuvenation Potion'
     #ret = py_droprec(cv2.imread('../../502/screens_1/95621693075.png'), y0=300, y1=345, x0=275, x1=465, signal=[[4+300, 10+275], [22+300, 62+275], [1+300, 1+275]])
     #ret = py_droprec(cv2.imread('../158458820669.png'), signal=[[145,245,361,'Martel De Fer','y'],[145,363,553,GHP,'w'],[145,555,745,GHP,'w'],[160,221,319,'Tusk Sword','g'],[160,321,526,FRP,'w'],[175,232,408,GMP,'w'],[190,222,351,'Studded Leather','b'],[205,226,381,SMP,'w'],[220,249,425,GMP,'w'],[235,176,366,GHP,'w'],[251,270,403,'Conquest Sword','y'],[268,207,397,GHP,'w']])
-    ret = py_droprec(*get_img('../../502/screens_1/94456900671.png'), signal=[[249,328,419,'Bone Wand','y'],[249,421,576,SMP,'w'],[249,578,783,FRP,'w'],[264,268,407,'Flawed Amethyst','w'],[264,409,574,RP,'w'],[279,307,476,SHP,'w'],[294,312,400,'Bone Shield','b'],[309,289,458,SHP,'w'],[324,326,491,RP,'w'],[339,244,434,GHP,'w'],[357,386,432,'Jewel','y'],[376,296,451,SMP,'w']])
+    # trained
+    ret = py_droprec(*get_img('../../502/screens_1/94456900671.png', signal=[[249,328,419,'Bone Wand','y'],[249,421,576,SMP,'w'],[249,578,783,FRP,'w'],[264,268,407,'Flawed Amethyst','w'],[264,409,574,RP,'w'],[279,307,476,SHP,'w'],[294,312,400,'Bone Shield','b'],[309,289,458,SHP,'w'],[324,326,491,RP,'w'],[339,244,434,GHP,'w'],[357,386,432,'Jewel','y'],[376,296,451,SMP,'w']]))
+    #
+    ret = py_droprec(*get_img('../../502/screens_1/95217013080.png'))
     print(ret)
   else:
     for img_name in list_images(sys.argv[1]):
